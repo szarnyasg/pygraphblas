0690bc7704efd5e68a70cb35857aefa752b7e9bb
marci543 <marci543@users.noreply.github.com>
2020-04-16 03:13:27 +0200

    Add test for LAGraph_bfs_pushpull

---
+++ tests/test_lagraph.py
@@ -0,0 +1,19 @@
+import pytest
+
+from pygraphblas import *
+from pygraphblas.base import lib, _check
+from pygraphblas.lagraph import LAGraph_bfs_pushpull
+
+
+def test_bfs_pushpull():
+    adj = Matrix.from_type(BOOL, 4, 4)
+    adj[0, 1] = True
+    adj[1, 2] = True
+
+    print(adj)
+    print(adj.to_string())
+
+    level, parent = LAGraph_bfs_pushpull(adj, None, 0, compute_parent=True)
+
+    print(level.to_string())
+    print(parent.to_string())
