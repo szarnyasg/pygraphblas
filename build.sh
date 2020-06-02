de3caf1af7b0b3c7c63507995cbb799982b4b807
marci543 <marci543@users.noreply.github.com>
2020-06-03 00:17:01 +0200

    Remove Docker push

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ BASE_NAME=$2
 SS_COMPACT=${SS_COMPACT:-0}
 
 docker build -f Dockerfile-${BASE_NAME} --build-arg SS_RELEASE=${SS_RELEASE} --build-arg SS_COMPACT=${SS_COMPACT} -t graphblas/pygraphblas-${BASE_NAME}:${SS_RELEASE} .
-docker push graphblas/pygraphblas-${BASE_NAME}:${SS_RELEASE}
+#docker push graphblas/pygraphblas-${BASE_NAME}:${SS_RELEASE}
 docker tag graphblas/pygraphblas-${BASE_NAME}:${SS_RELEASE} graphblas/pygraphblas-${BASE_NAME}:latest
-docker push graphblas/pygraphblas-${BASE_NAME}:latest
+#docker push graphblas/pygraphblas-${BASE_NAME}:latest
